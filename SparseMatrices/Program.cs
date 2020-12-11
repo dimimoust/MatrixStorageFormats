c9c8bf5f9aa76a54c29e7388ef47d978f4a36a1d
Dimitra Moustogianni <dim.moust@gmail.com>
2020-12-11 21:09:54 +0200

    Skyline (constructor returns values and diagonaloffsets)

--- SparseMatrices/Program.cs
+++ SparseMatrices/Program.cs
@@ -7,67 +7,67 @@ namespace SparseMatrices
     {
         static void Main()
         {
-            double[,] matrix = { { 1, 2, 3 }, { 0, 2, 0 }, { 0, 5, 3 } };
-            double[] vector = { 1, 2, 3 };
-            MatrixOperations multiplication = new MatrixOperations(matrix, vector);
-            Console.WriteLine($"Multiplication result is = {multiplication.Result}");
-            /*double[] matrixp= { 8, 2, 3, 5, 4, 6, };
-            double[,] matrix = { { 1, 2, 3 }, { 0, 2, 0 }, { 0, 5,3 } };
-            double[,] skymatrix = { { 1, 0, 1, 0, 8,5 }, { 0, 2, 0, 1, 0,0 }, { 1, 0, 3, 1, 0, 1}, { 0, 1, 1, 4, 0,0 }, { 8, 0, 0, 0, 5,1 },{5,0,4,0,1,6} };
-            //double[,] skymatrix = {{1,-1,-3,0,0},{-2,5,0,0,0},{0,0,4,6,4},{-4,0,2,7,0},{0,8,0,0,-5}};
-            double[,] banmatrix =
-            {
-                {1, 0, 1, 0, 8, 5, 0, 0, 0, 0, 0},
-                {0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-                {1, 0, 3, 1, 0, 1, 0, 0, 0, 0, 0},
-                {0, 1, 1, 4, 0, 0, 0, 0, 0, 0, 0},
-                {8, 0, 0, 0, 5, 1, 0, 0, 0, 0, 0},
-                {5, 0, 4, 0, 1, 6, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            };
-            double[,] band2 =
-            {
-                {1, 2, 0, 3, 0, 0, 0, 0},
-                {2, 4, 5, 1, 0, 0, 0, 0},
-                {0, 5, 6, 1, 0, 1, 0, 0},
-                {3, 1, 1, 7, 0, 0, 0, 0},
-                {0, 0, 0, 0, 8, 1, 0, 0},
-                {0, 0, 1, 0, 1, 9, 0, 0},
-                {0, 0, 0, 0, 0, 0, 10, 0},
-                {0, 0, 0, 0, 0, 0, 0, 11},
-            };
-            double[] vector = {2, 4, 5,2};
-            CompressedSparseColumns arrayCSR = new CompressedSparseColumns();
-            double[] CSC = arrayCSR.CSCvectorMultiplication(vector, matrix);
-            PackedStorage matrixrow = new PackedStorage();
-           // double[] result = matrixrow.MultiplyWithVector(matrixp, vector);
-            double[,] matrixD = { { 1, 0, 0 }, { 0, 5, 0 }, { 0, 0, 2 } };
-            double[,] diagonal = { { 1, 0, 0,4 }, { 0, 5, 3,5 }, { 0, 3, 6,0 } ,{4,5,0,8}};
-            double[,] csR = {{9, 0, 3, 0}, {0, 8, 0, 0}, {0, 2, 6, 0}, {1, 0, 0, 5}};
-            Banded pinakas = new Banded();
+            double[,] skymatrix = { { 1, 0, 1, 0, 8, 5 }, { 0, 2, 0, 1, 0, 0 }, { 1, 0, 3, 1, 0, 1 }, { 0, 1, 1, 4, 0, 0 }, { 8, 0, 0, 0, 5, 1 }, { 5, 0, 4, 0, 1, 6 } };
+            Skyline skyline = new Skyline(skymatrix);
+            Console.WriteLine($"The products of Skyline Storage are Values = {skyline.Values} and DiagOffSets = {skyline.DiagOffSets} ");
+            //double[,] matrix = { { 1, 2, 3 }, { 0, 2, 0 }, { 0, 5, 3 } };
+            //double[] vector = { 1, 2, 3 };
+            //MatrixOperations multiplication = new MatrixOperations(matrix, vector);
+            //Console.WriteLine($"Multiplication result is = {multiplication.Result}");
+            //double[] matrixp = { 8, 2, 3, 5, 4, 6, };
+            //double[,] matrix = { { 1, 2, 3 }, { 0, 2, 0 }, { 0, 5, 3 } };
+            //double[,] skymatrix = { { 1, 0, 1, 0, 8, 5 }, { 0, 2, 0, 1, 0, 0 }, { 1, 0, 3, 1, 0, 1 }, { 0, 1, 1, 4, 0, 0 }, { 8, 0, 0, 0, 5, 1 }, { 5, 0, 4, 0, 1, 6 } };
+            //double[,] banmatrix =
+            //{
+            //    {1, 0, 1, 0, 8, 5, 0, 0, 0, 0, 0},
+            //    {0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+            //    {1, 0, 3, 1, 0, 1, 0, 0, 0, 0, 0},
+            //    {0, 1, 1, 4, 0, 0, 0, 0, 0, 0, 0},
+            //    {8, 0, 0, 0, 5, 1, 0, 0, 0, 0, 0},
+            //    {5, 0, 4, 0, 1, 6, 0, 0, 0, 0, 0},
+            //    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //};
+            //double[,] band2 =
+            //{
+            //    {1, 2, 0, 3, 0, 0, 0, 0},
+            //    {2, 4, 5, 1, 0, 0, 0, 0},
+            //    {0, 5, 6, 1, 0, 1, 0, 0},
+            //    {3, 1, 1, 7, 0, 0, 0, 0},
+            //    {0, 0, 0, 0, 8, 1, 0, 0},
+            //    {0, 0, 1, 0, 1, 9, 0, 0},
+            //    {0, 0, 0, 0, 0, 0, 10, 0},
+            //    {0, 0, 0, 0, 0, 0, 0, 11},
+            //};
+            //double[] vector = { 2, 4, 5, 2 };
+            //CompressedSparseColumns arrayCSR = new CompressedSparseColumns();
+            //double[] CSC = arrayCSR.CSCvectorMultiplication(vector, matrix);
+            //PackedStorage matrixrow = new PackedStorage();
+            //double[] result = matrixrow.MultiplyWithVector(matrixp, vector);
+            //double[,] matrixD = { { 1, 0, 0 }, { 0, 5, 0 }, { 0, 0, 2 } };
+            //double[,] diagonal = { { 1, 0, 0, 4 }, { 0, 5, 3, 5 }, { 0, 3, 6, 0 }, { 4, 5, 0, 8 } };
+            //double[,] csR = { { 9, 0, 3, 0 }, { 0, 8, 0, 0 }, { 0, 2, 6, 0 }, { 1, 0, 0, 5 } };
+            //Banded pinakas = new Banded();
 
-            var (res,res1, _, _) = pinakas.BandedStorage(band2);
-            PackedStorage array = new PackedStorage();
-           ( double[],int[],int[]) packedArrayColumn = array.Row_Major_Layout(diagonal);
-            double[] packedArrayRow = array.MultiplyWithVector(diagonal,vector);
-            Diagonal arrayD = new Diagonal();
-            double[] diagonalStorage = arrayD.DiagonalVectorMultiplication(vector, matrixD);
-            CoordinateList arraycl = new CoordinateList();
-            double[] result2 = arraycl.CoordListvectorMultiplication(vector,matrix);
-            var (values, _, columns) = arraycl.CoordinateListStorage(matrix);
-            CompressedSparseColumns arrayCsr = new CompressedSparseColumns();
-            (double[], int[] , int[]) csr = arrayCsr.CSCstorage(csR);
-            DictionaryOfKeys arrayDk = new DictionaryOfKeys();
-            Dictionary<int, Dictionary<int, double>> dok = arrayDk.DOK(matrix);
-            Skyline arrayS = new Skyline();
-            double[,] skyline = arrayS.SkylineStorage(skymatrix);
+            //var (res, res1, _, _) = pinakas.BandedStorage(band2);
+            //PackedStorage array = new PackedStorage();
+            //(double[], int[], int[]) packedArrayColumn = array.Row_Major_Layout(diagonal);
+            //double[] packedArrayRow = array.MultiplyWithVector(diagonal, vector);
+            //Diagonal arrayD = new Diagonal();
+            //double[] diagonalStorage = arrayD.DiagonalVectorMultiplication(vector, matrixD);
+            //CoordinateList arraycl = new CoordinateList();
+            //double[] result2 = arraycl.CoordListvectorMultiplication(vector, matrix);
+            //var (values, _, columns) = arraycl.CoordinateListStorage(matrix);
+            //CompressedSparseColumns arrayCsr = new CompressedSparseColumns();
+            //(double[], int[], int[]) csr = arrayCsr.CSCstorage(csR);
+            //DictionaryOfKeys arrayDk = new DictionaryOfKeys();
+            //Dictionary<int, Dictionary<int, double>> dok = arrayDk.DOK(matrix);
 
-            var matrixFull = new Matrix(diagonal);
-            //matrixFull[0, 0]*/
+            //var matrixFull = new Matrix(diagonal);
+            //matrixFull[0, 0]
         }
     }
 }
--- SparseMatrices/Skyline.cs
+++ SparseMatrices/Skyline.cs
@@ -6,37 +6,100 @@ namespace SparseMatrices
 {
     public class Skyline
     {
-        public Skyline()
+        private readonly double[,] Matrix;
+        private double[] values;
+        private int[] diagOffSets;
+
+        public double[] Values
+        {
+            get => values;
+            set => values = value;
+        }
+        public int[] DiagOffSets
+        {
+            get => diagOffSets;
+            set => diagOffSets = value;
+        }
+
+        public Skyline(double[,] matrix)
         {
             _listValues = new List<double>();
-            _listDiagOffsets = new List<int>();
+            this.Matrix = matrix;
+            var (_, skyValues ,skyDiagOffsets) = SkylineStorage(matrix);
+            this.Values = skyValues;
+            this.DiagOffSets = skyDiagOffsets;
         }
-        public double[] SkylineStorage(double[,] matrix)
+
+        public double this[int i, int j]
+        {
+            get => Matrix[i, j];
+            set => Matrix[i, j] = value;
+        }
+
+        public (double[,] ,double[] , int[] ) SkylineStorage(double[,] matrix)
         {
             int rows = matrix.GetLength(0);
             int columns = matrix.GetLength(1);
-            double[,] matrixSkyLine = new double[rows,columns];
-            double[] activeColumn = new double[columns];
-            int n = 0;
+            double[,] matrixSkyLine = new double[rows, columns];
+            int[] activeColumn = new int[columns];
+            int[] height = new int[columns];
+
+            for (int indexColumn = 0; indexColumn < columns; indexColumn++)
+            {
+                int count = 0;
+                for (int indexRow = 0; indexRow < indexColumn; indexRow++)
+                {
+                    if (matrix[indexRow, indexColumn] == 0)
+                    {
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                height[indexColumn] = count;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                sum += height[i];
+            }
+
+            int totalvalues = 0;
             for (int indexRow = 0; indexRow < rows; indexRow++)
             {
-                for (int indexColumn = indexRow; indexColumn >= 0; indexColumn--)
+                for (int indexColumn = 0; indexColumn <= indexRow; indexColumn++)
                 {
-                    if (matrix[indexRow, indexColumn] != 0)
-                    {
-                        _listValues.Add(matrix[indexRow, indexColumn]);
-                        if (indexRow == indexColumn)
-                        {
-                            _listDiagOffsets.Add(n);
-                        }
-                        n++;
-                    }
+                    totalvalues++;
                 }
             }
 
-            _listDiagOffsets.Add(_listValues.Count);
-            int[] diagOffsets = _listDiagOffsets.ToArray();
+            if (matrix[0, 0] != 0)
+            {
+                _listValues.Add(matrix[0, 0]);
+            }
+            for (int indexRow = 1; indexRow < rows; indexRow++)
+            {
+                for (int indexColumn = indexRow; indexColumn >= height[indexColumn] && indexColumn > 0; indexColumn--)
+                {
+                    _listValues.Add(matrix[indexRow, indexColumn]);
+                }
+                if (matrix[0, indexRow] != 0)
+                {
+                    _listValues.Add(matrix[0, indexRow]);
+                }
+            }
+
             double[] values = _listValues.ToArray();
+            int[] diagOffsets = new int[columns + 1];
+            diagOffsets[columns] = totalvalues - sum;
+            for (int indexRow = 1; indexRow < rows; indexRow++)
+            {
+                diagOffsets[indexRow] = diagOffsets[indexRow - 1] + (indexRow - height[indexRow - 1]);
+            }
 
             for (int j = 0; j < columns; j++)
             {
@@ -65,14 +128,15 @@ namespace SparseMatrices
                             matrixSkyLine[i, j] = 0;
                         }
                     }
+
                     matrixSkyLine[j, i] = matrixSkyLine[i, j];
                 }
             }
-            return values;
+
+            return (matrixSkyLine, values , diagOffsets);
         }
 
         List<double> _listValues;
-        List<int> _listDiagOffsets;
     }
 }
 
